0814379bea2d008271de0d821eb6a815e05a610e
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-06 17:26:24 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { Container, Typography, Box, Button, Grid } from "@material-ui/core/";
-
+import Head from "next/head";
+import styles from "../styles/Layout.module.css";
 export default function About() {
 	return (
-		<Grid>
-			<Container>
-				<Box my={4}>
-					<Typography variant="h4" component="h1" gutterBottom>
-						Next.js example
-					</Typography>
-					<Button variant="contained" color="primary" naked href="/">
-						Go to the main page
-					</Button>
-				</Box>
-			</Container>
-		</Grid>
+		<div className={styles.container}>
+			<Head>
+				<title>About| SIFK</title>
+				<link rel="icon" href="/favicon32.png" />
+			</Head>
+			<main className={styles.main}>
+				<h3 className={styles.title}>
+					I began my streaming journey last year right around when pandemic
+					started.
+				</h3>
+				<h3 className={styles.title}>
+					I mainly play Hunt Showdown, Warzone and recently started playing
+					Rust.
+				</h3>
+			</main>
+		</div>
 	);
 }
