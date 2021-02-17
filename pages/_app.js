a36268279427fdc550a88d67c8b4a880b5a91271
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-17 15:52:05 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import "../styles/globals.css";
 import Layout from "../comps/Layout";
-import Meta from "../comps/Meta";
-import Head from "next/head";
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
-			{/* <Head>
-				<link rel="icon" type="image/png/x-icon" href="/favicon.png" />
-			</Head> */}
 			<Component {...pageProps} />
 		</Layout>
 	);
