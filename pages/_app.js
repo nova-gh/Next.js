380e907f4d53181f21be3fe5fa7b689f104c1623
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-17 10:14:49 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import Layout from "../comps/Layout";
-import Meta from "../comps/Meta";
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
