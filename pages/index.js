73dde8e80ffe1a86ecd5dcd419a1a8d554042003
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-16 19:07:54 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import styles from "../styles/Layout.module.css";
+import styles from "../styles/home.module.css";
 import Avatar from "avataaars";
 import { Container } from "@material-ui/core";
+
 export default function Home() {
 	return (
 		<div className={styles.container}>
@@ -9,10 +10,10 @@ export default function Home() {
 				<title>Sifk | Twitch Streamer</title>
 				<link rel="icon" href="/favicon32.png" />
 			</Head>
+
 			<main className={styles.main}>
-				<Container>
+				<grid className={styles.grid}>
 					<Avatar
-						avatarStyle="Circle"
 						topType="ShortHairTheCaesar"
 						accessoriesType="Blank"
 						hairColor="Black"
@@ -24,13 +25,14 @@ export default function Home() {
 						eyebrowType="Default"
 						mouthType="Smile"
 						skinColor="DarkBrown"
-						className=""
+						className={styles.avatar}
 					/>
-				</Container>
-				<h1 className={styles.title}>
-					Hi!, my name is Sifk, a
-					<a href="https://www.twitch.tv/sifk"> Twitch Streamer!</a>
-				</h1>
+					<h1 className={styles.title}>Hello!</h1>
+					<h1 className={styles.title}>
+						My name is Sifk, a {}
+						<a href="https://www.twitch.tv/sifk">Twitch Streamer!</a>
+					</h1>
+				</grid>
 			</main>
 		</div>
 	);
