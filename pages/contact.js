aedbc09fb2498d124169353fb4fef454930fbb97
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-17 15:29:49 -0800

    Implemented Container from bootstrap

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,7 @@ import {
 	faTwitter,
 	faDiscord,
 } from "@fortawesome/free-brands-svg-icons";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 export default function Contact() {
 	return (
 		<div className={styles.wrapper}>
@@ -15,49 +15,55 @@ export default function Contact() {
 				title="Contact"
 				desc="Sifk's Twitch Streamer. Come say hello in the chat."
 			/>
-			<main className={styles.main}>
-				<Row>
-					<Col className={styles.socialContact}>
-						<a
-							href="https://www.twitch.tv/sifk"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<FontAwesomeIcon
-								icon={faTwitch}
-								style={{ color: "#9147ff" }}
-								size="5x"
-							/>
-						</a>
+			<Container>
+				<Row className={styles.contactRow}>
+					<Col className>
+						<Container className={styles.socialCont}>
+							<a
+								href="https://www.twitch.tv/sifk"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faTwitch}
+									style={{ color: "#9147ff" }}
+									size="xs"
+								/>
+							</a>
+						</Container>
 					</Col>
-					<Col className={styles.socialContact}>
-						<a
-							href="https://twitter.com/SIFKontwitch"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<FontAwesomeIcon
-								icon={faTwitter}
-								style={{ color: "#1da1f2" }}
-								size="5x"
-							/>
-						</a>
+					<Col className>
+						<Container className={styles.socialCont}>
+							<a
+								href="https://twitter.com/SIFKontwitch"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faTwitter}
+									style={{ color: "#1da1f2" }}
+									size="1x"
+								/>
+							</a>
+						</Container>
 					</Col>
-					<Col className={styles.socialContact}>
-						<a
-							href="https://discord.com/invite/NjpVcEu"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<FontAwesomeIcon
-								style={{ color: "#7289da" }}
-								icon={faDiscord}
-								size="5x"
-							/>
-						</a>
+					<Col className>
+						<Container className={styles.socialCont}>
+							<a
+								href="https://discord.com/invite/NjpVcEu"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									style={{ color: "#7289da" }}
+									icon={faDiscord}
+									size="1x"
+								/>
+							</a>
+						</Container>
 					</Col>
 				</Row>
-			</main>
+			</Container>
 		</div>
 	);
 }
