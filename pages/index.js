92b80a7132ca127ff2042f6908978816d5422eb3
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-17 12:30:08 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import Image from "next/image";
 import styles from "../styles/home.module.css";
 import Avatar from "avataaars";
 import Meta from "../comps/Meta";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 export default function Home() {
 	return (
 		<div className={styles.container}>
@@ -10,9 +11,10 @@ export default function Home() {
 				desc="Sifk's Twitch Streamer. Come say hello in the chat."
 			/>
 
-			<main className={styles.main}>
-				<Row>
-					<Avatar
+			{/* <main className={styles.main}> */}
+			<Container>
+				<Row className={styles.homeRow}>
+					{/* <Avatar
 						topType="ShortHairTheCaesar"
 						accessoriesType="Blank"
 						hairColor="Black"
@@ -25,18 +27,27 @@ export default function Home() {
 						mouthType="Smile"
 						skinColor="DarkBrown"
 						className={styles.avatar}
+					/> */}
+					<Image
+						src="/avatar.svg"
+						// src={logo}
+						alt="avatar"
+						width={200}
+						height={200}
+						className={styles.avatarHome}
 					/>
 				</Row>
-				<Row>
+				<Row className={styles.homeRow}>
 					<h1 className={styles.title}>Hello!</h1>
 				</Row>
-				<Row>
+				<Row className={styles.homeRow}>
 					<h1 className={styles.title}>
 						My name is Sifk, a {}
 						<a href="https://www.twitch.tv/sifk">Twitch Streamer!</a>
 					</h1>
 				</Row>
-			</main>
+			</Container>
+			{/* </main> */}
 		</div>
 	);
 }
