a7c6568af7c7bcbe1156b0ea333179ff43aebdf9
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-17 16:19:11 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,39 @@
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
+// import styles from "../styles/Home.module.css";
+// import Image from "next/image";
 
-import Meta from "../comps/Meta";
-import { Row, Col, Container } from "react-bootstrap";
+// import Meta from "../comps/Meta";
+// import { Row, Col, Container } from "react-bootstrap";
 export default function Home() {
 	return (
-		<div className={styles.wrapper}>
-			<Meta
-				title="Home"
-				desc="Sifk's Twitch Streamer. Come say hello in the chat."
-			/>
-
-			<Container>
-				<Row className={styles.homeRow}>
-					<Image
-						src="/avatar.svg"
-						alt="avatar"
-						width={200}
-						height={200}
-						className={styles.avatarHome}
-					/>
-				</Row>
-				<Row className={styles.homeRow}>
-					<h1 className={styles.title}>Hello!</h1>
-				</Row>
-				<Row className={styles.homeRow}>
-					<h1 className={styles.title}>
-						My name is Sifk, a {}
-						<a href="https://www.twitch.tv/sifk">Twitch Streamer.</a>
-					</h1>
-				</Row>
-			</Container>
+		<div>
+			<h1>hello</h1>
 		</div>
+		// <div className={styles.wrapper}>
+		// 	<Meta
+		// 		title="Home"
+		// 		desc="Sifk's Twitch Streamer. Come say hello in the chat."
+		// 	/>
+
+		// 	<Container>
+		// 		<Row className={styles.homeRow}>
+		// 			<Image
+		// 				src="/avatar.svg"
+		// 				alt="avatar"
+		// 				width={200}
+		// 				height={200}
+		// 				className={styles.avatarHome}
+		// 			/>
+		// 		</Row>
+		// 		<Row className={styles.homeRow}>
+		// 			<h1 className={styles.title}>Hello!</h1>
+		// 		</Row>
+		// 		<Row className={styles.homeRow}>
+		// 			<h1 className={styles.title}>
+		// 				My name is Sifk, a {}
+		// 				<a href="https://www.twitch.tv/sifk">Twitch Streamer.</a>
+		// 			</h1>
+		// 		</Row>
+		// 	</Container>
+		// </div>
 	);
 }
