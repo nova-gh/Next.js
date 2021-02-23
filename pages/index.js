af579771eb0f444ae45d24e1c390b1e442d9146a
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-22 23:43:20 -0800

    added the live status components.

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,20 @@ import styles from "../styles/Index.module.css";
 import Image from "next/image";
 
 import Meta from "../comps/Meta";
-import Live from "../comps/Live";
+import Online from "../comps/Online";
+import Offline from "../comps/Offline";
+
 import { Row, Col, Container } from "react-bootstrap";
-export default function Home() {
+export default function Home({ results }) {
+	const apiResults = results.data;
+	console.log(apiResults);
+	var statusButton;
+	if (!Array.isArray(apiResults) || !apiResults.length) {
+		statusButton = <Offline />;
+	} else {
+		statusButton = <Online />;
+	}
+
 	return (
 		<div className={styles.wrapper}>
 			<Meta
@@ -32,8 +43,23 @@ export default function Home() {
 						<a href="https://www.twitch.tv/sifk">Twitch Streamer.</a>
 					</h1>
 				</Row>
-				<Row className={styles.homeRow}>{/* live */}</Row>
+				<Row className={styles.homeRow}>{statusButton}</Row>
 			</Container>
 		</div>
 	);
+}
+export async function getServerSideProps() {
+	// console.log(process.env.AUTH);
+	// console.log(process.env.CID);
+	const res = await fetch(process.env.API_URL, {
+		headers: {
+			Authorization: process.env.AUTH,
+			"Client-Id": process.env.CID,
+		},
+	});
+	const results = await res.json();
+	// console.log(results);
+	return {
+		props: { results },
+	};
 }
