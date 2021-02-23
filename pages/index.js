f7c5a646907c28a72fae0ff5dfc34c87910ac917
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-23 13:30:22 -0800

    updated css

--- pages/index.js
+++ pages/index.js
@@ -52,48 +52,23 @@ const Home = ({ results }) => {
 					<h1 className={styles.title}>Hello!</h1>
 				</Row>
 				<Row className={styles.homeRow}>
+					<h1 className={styles.title}>My name is Sifk,</h1>
+				</Row>
+				<Row className={styles.homeRow}>
 					<h1 className={styles.title}>
-						My name is Sifk, a {}
-						<a href="https://www.twitch.tv/sifk">Twitch Streamer.</a>
+						<a
+							href="https://www.twitch.tv/sifk"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Twitch Streamer.
+						</a>
 					</h1>
 				</Row>
-				<Row className={styles.homeRow}>{statusButton}</Row>
+
+				<Row className={styles.statusRow}>{statusButton}</Row>
 			</Container>
 		</div>
 	);
 };
 export default Home;
-
-//depolycode
-// export default function Home() {
-// 	return (
-// 		<div className={styles.wrapper}>
-// 			<Meta
-// 				title="Home"
-// 				desc="Sifk's Twitch Streamer. Come say hello in the chat."
-// 			/>
-
-// 			<Container>
-// 				<Row className={styles.homeRow}>
-// 					<Image
-// 						src="/avatar.svg"
-// 						alt="avatar"
-// 						width={200}
-// 						height={200}
-// 						className={styles.avatarHome}
-// 					/>
-// 				</Row>
-// 				<Row className={styles.homeRow}>
-// 					<h1 className={styles.title}>Hello!</h1>
-// 				</Row>
-// 				<Row className={styles.homeRow}>
-// 					<h1 className={styles.title}>
-// 						My name is Sifk, a {}
-// 						<a href="https://www.twitch.tv/sifk">Twitch Streamer.</a>
-// 					</h1>
-// 				</Row>
-// 				{/* <Row className={styles.homeRow}>{statusButton}</Row> */}
-// 			</Container>
-// 		</div>
-// 	);
-// }
