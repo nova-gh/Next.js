46e50b7ad350fc3cecfa4d697cd2d4af9f1bea9d
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-23 13:10:42 -0800

    changed css for status

--- comps/Offline.js
+++ comps/Offline.js
@@ -1,7 +1,13 @@
-import { Button } from "react-bootstrap";
-
+import { Button, Container } from "react-bootstrap";
+import styles from "../styles/Layout.module.css";
 const Offline = () => {
-	return <Button variant="light">Offline</Button>;
+	return (
+		<Button variant="light">
+			<div className={styles.offlineCont}>
+				<span class={styles.offlineCirlce}></span>
+			</div>
+			Offline
+		</Button>
+	);
 };
-
 export default Offline;
--- comps/Online.js
+++ comps/Online.js
@@ -1,7 +1,21 @@
-import { Button } from "react-bootstrap";
-
+import { Button, Spinner } from "react-bootstrap";
+import styles from "../styles/Layout.module.css";
 const Online = () => {
-	return <Button variant="danger">Live!</Button>;
+	return (
+		<Button variant="danger">
+			<div className={styles.onlineCont}>
+				<Spinner
+					className={styles.onlineCirlce}
+					as="span"
+					animation="grow"
+					size="sm"
+					role="Online Status"
+					aria-hidden="true"
+				/>
+			</div>
+			Live!
+		</Button>
+	);
 };
 
 export default Online;
