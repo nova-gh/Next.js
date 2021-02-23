d7565dda10054e921fedb9faba49b6f897512bdc
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-22 23:42:27 -0800

    Created Live Status Component

---
+++ comps/Offline.js
@@ -0,0 +1,7 @@
+import { Button } from "react-bootstrap";
+
+const Offline = () => {
+	return <Button variant="danger">Offline</Button>;
+};
+
+export default Offline;
---
+++ comps/Online.js
@@ -0,0 +1,7 @@
+import { Button } from "react-bootstrap";
+
+const Online = () => {
+	return <Button variant="danger">Live!</Button>;
+};
+
+export default Online;
