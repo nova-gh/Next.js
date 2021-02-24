9232157e38bf63fd4d4f75b4ac92c5878efda244
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-23 16:53:55 -0800

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Head from "next/head";
 import styles from "../styles/About.module.css";
 import Meta from "../comps/Meta";
+import { Container, Row, Card, CardDeck, Col, Media } from "react-bootstrap";
+import Image from "next/image";
+// import Yo from "";
 export default function About() {
 	return (
 		<div className={styles.wrapper}>
@@ -9,16 +11,59 @@ export default function About() {
 				title="About"
 				desc="Sifk's Twitch Streamer. Come say hello in the chat."
 			/>
-			<main className={styles.main}>
-				<h3 className={styles.title}>
-					I began my streaming journey last year right around when pandemic
-					started.
-				</h3>
-				<h3 className={styles.title}>
-					I mainly play Hunt Showdown, Warzone and recently started playing
-					Rust.
-				</h3>
-			</main>
+			<Container className={styles.aboutCont}>
+				<Media className="pb-4">
+					<img
+						className="mr-3 rounded"
+						width={120}
+						height={120}
+						alt="yosemite"
+						src="/yosemite.jpg"
+					/>
+					<Media.Body>
+						<h5 className={styles.title}>About me</h5>
+						<p className={styles.text}>
+							I am based of West Coast and currently a student majoring in
+							Economics. As the pandemic started last year, I decided to start
+							streaming on Twitch TV.
+						</p>
+					</Media.Body>
+				</Media>
+				<Media className="pb-4">
+					<Media.Body>
+						<h5 className={styles.title}>Hobbies</h5>
+						<p className={styles.text}>
+							When I am not streaming, I like to stay active. My favorite way to
+							stay active would be doing Jui Jitsu or playing soccer.
+						</p>
+					</Media.Body>
+					<img
+						className="ml-3 rounded"
+						width={120}
+						height={120}
+						alt="yosemite"
+						src="/ju.JPG"
+					/>
+				</Media>
+				<Media className="pb-4">
+					<img
+						className="mr-3 rounded"
+						width={120}
+						height={120}
+						alt="yosemite"
+						src="/hunt.jpg"
+					/>
+					<Media.Body>
+						<h5 className={styles.title}>Streaming Content</h5>
+						<p className={styles.text}>
+							As of right now I am mainly playing Hunt Show Down and enjoying
+							making content with other like minded streamers. I also play and
+							stream other games such as Call of Duty: Warzone, League of
+							Legends, Rust and much more.
+						</p>
+					</Media.Body>
+				</Media>
+			</Container>
 		</div>
 	);
 }
