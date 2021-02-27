9d45d2b76742bdd07476108afaa48118b2c2fca4
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-26 16:53:30 -0800

    Revert "test"
    This reverts commit bae7973fbbf05f00e16df7909b7096ff0927585b.

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,7 @@ const Home = ({ results }) => {
 				title="Home"
 				desc="Sifk's Twitch Streamer. Come say hello in the chat."
 			/>
+
 			<Container>
 				<Row className={styles.homeRow}>
 					<Image
