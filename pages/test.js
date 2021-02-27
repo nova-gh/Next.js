babb9e2d5bf345d24deb27e513cf773b6c0c5e01
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2021-02-26 18:43:15 -0800

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,28 +1,28 @@
-import { Button } from "react-bootstrap";
-const test = ({ results }) => {
-	let apiResults = results.data;
-	// console.log(apiResults);
-	if (!Array.isArray(apiResults) || !apiResults.length) {
-		// console.log("Offline");
-		return <Button variant="light">Offline</Button>;
-	} else {
-		return <Button variant="danger">LIVE</Button>;
-		// console.log("Live");
-	}
-};
-export async function getServerSideProps() {
-	// console.log(process.env.AUTH);
-	// console.log(process.env.CID);
-	const res = await fetch(process.env.API_URL, {
-		headers: {
-			Authorization: process.env.AUTH,
-			"Client-Id": process.env.CID,
-		},
-	});
-	const results = await res.json();
-	// console.log(results);
-	return {
-		props: { results },
-	};
-}
-export default test;
+// import { Button } from "react-bootstrap";
+// const test = ({ results }) => {
+// 	let apiResults = results.data;
+// 	// console.log(apiResults);
+// 	if (!Array.isArray(apiResults) || !apiResults.length) {
+// 		// console.log("Offline");
+// 		return <Button variant="light">Offline</Button>;
+// 	} else {
+// 		return <Button variant="danger">LIVE</Button>;
+// 		// console.log("Live");
+// 	}
+// };
+// export async function getServerSideProps() {
+// 	// console.log(process.env.AUTH);
+// 	// console.log(process.env.CID);
+// 	const res = await fetch(process.env.API_URL, {
+// 		headers: {
+// 			Authorization: process.env.AUTH,
+// 			"Client-Id": process.env.CID,
+// 		},
+// 	});
+// 	const results = await res.json();
+// 	// console.log(results);
+// 	return {
+// 		props: { results },
+// 	};
+// }
+// export default test;
